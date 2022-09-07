3394f973c635559619a012046bca640dcf1faf64
0knotok <20213134@aloe.ulima.edu.pe>
2022-09-07 17:44:57 -0500

    nav linkeado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
         <div className="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <Link to='/portal'>
-                        <a class="navbar-brand" href="#">E-Learn</a>
+                    <Link to='/portal' className="navbar-brand">
+                        E-Learn
                     </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -15,10 +15,7 @@ const Navbar = () => {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Módulos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Foro</a>
+                                <Link className="nav-link" to="/foro">Foro</Link>
                             </li>
                         </ul>
                     </div>
