794343c77aa50f325d1d489683f0b1c0f27850f6
CesarGalarza <20190779@aloe.ulima.edu.pe>
2022-10-27 17:44:38 -0500

    Todo Crear, Dar, Revisar Examen y los factory del foro y preguntas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Portal from './pages/Portal';
-import LogIn from './pages/login';
-import SignUpAl from './pages/signupAl';
-import SignUpPr from './pages/signupPR';
-import NotFound from './components/NotFound';
-import Perfil from './pages/Perfil';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PortalAlumno from './pages/PortalAlumno'
+import LogIn from './pages/login'
+import SignUpAl from './pages/signupAl'
+import SignUpPr from './pages/signupPR'
+import NotFound from './components/NotFound'
+import Perfil from './pages/Perfil'
 import Curso from './pages/ModuloCurso'
-import ForoPagina from './pages/foro';
-import Disponibilidad from './pages/disponiblidad';
-import './App.css';
-import EvaProfe from './pages/evaProfe';
-import CrearExamen from './pages/crearExamen';
-import Examen from './pages/examen';
-import RevisarExamen from './pages/revisarExamen';
+import ForoPagina from './pages/foro'
+import Disponibilidad from './pages/disponiblidad'
+import RecContra from './pages/recuperacionContra'
+import SubirArchivo from './components/SubirArchivo'
+import './App.css'
+import EvaProfe from './pages/evaProfe'
+import CrearEvaluacion from './pages/crearEvaluacion'
+import CreacionFormulario from './pages/CreacionFormulario'
+import FormularioEvaluacion from './pages/FormularioEvaluacion'
+import PortalProfesor from './pages/PortalProfesor'
+import CrearExamen from './pages/crearExamen'
+import Examen from './pages/examen'
+import RevisarExamen from './pages/revisarExamen'
+import PortalAdministrador from './pages/PortalAdministrador'
 
 
 function App() {
@@ -21,7 +28,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/portal' element={<Portal/>}/>
+          <Route path='/PortalAlumno' element={<PortalAlumno/>}/>
+          <Route path='/PortalProfesor' element={<PortalProfesor/>}/>
           <Route path='/curso' element={<Curso/>}/>
           <Route path='/foro' element={<ForoPagina/>}/>
           <Route path='/crearexamen' element={<CrearExamen/>}/>
@@ -32,8 +40,14 @@ function App() {
           <Route path='/signUpA' element={<SignUpAl/>}/>
           <Route path='/signUpP' element={<SignUpPr/>}/>
           <Route path='/evaProfe' element={<EvaProfe/>}/>
-          <Route path='/perfil' element={<Perfil/>}/>
+          <Route path='/crearEvaluacion' element={<CrearEvaluacion/>}/>
+          <Route path='/Perfil' element={<Perfil/>}/>
+          <Route path='/recuperar' element={<RecContra/>}/>
           <Route path='/disponibilidad' element={<Disponibilidad/>}/>
+          <Route path='/subirArchivo' element={<SubirArchivo/>}/>
+          <Route path='/CreacionFormulario' element={<CreacionFormulario/>}/>
+          <Route path='/FormularioEvaluacion' element={<FormularioEvaluacion/>}/>
+          <Route path='/PortalAdministrador' element={<PortalAdministrador/>}/>
       </Routes>
     </BrowserRouter>
     {/*<UserConfigModal/>*/}
--- src/components/Curso.js
+++ src/components/Curso.js
@@ -1,27 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-export default function Curso() {
+export default function Curso(props) {
+
   return (
     <div className="card m-2">
     <div className="card-body">
-      <h5 className="card-title text-center mb-5">Course Name</h5>
+
+      <h5 className="card-title text-center mb-5 mt-3">{props.nombre}</h5>
       <div className="progress">
-        <div className="progress-bar" role="progressbar" style={{width: '25%'}} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100}>25%</div>
+      
+        <div className="progress-bar" role="progressbar" style={{width: `${props.ptj}`}} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100}>{props.ptj}</div>
       </div>
-      <div className="mt-4 mb-2 d-flex justify-content-between">
-        <a href="#" className="btn btn-light" style={{width: '48%'}}>See summary</a>
-        <Link to='/curso'>
-          <a href="#" className="btn btn-light" style={{width: '100%'}}>Go to the course</a>
+      <div className="mt-4 mb-2 d-flex justify-content-between text-center">
+        <Link to={'/curso?id_curso='+ props.id+'&nombre_curso='+props.nombre}>
+          <a className="btn btn-light text-center" style={{width: '100%'}}>Go to the course</a>
         </Link>
       </div>
       <img className="card-img-top" src="../img/illustration-teacher-students.jpeg" alt="Card image cap" />
     </div>
-    <Link to='/evaProfe'>
-      <span className='small'>Review your teacher</span>
-    </Link>
-
-  </div>
-
-
-  )
+  </div>
+  )
 }
